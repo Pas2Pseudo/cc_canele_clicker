a2077f9e45866bfae01bd3f69ae18063b1dd30cf
Pas2Pseudo <80533655+Pas2Pseudo@users.noreply.github.com>
2022-01-29 13:57:13 +0100

    Add local cache

--- ressource/script/cuisson.js
+++ ressource/script/cuisson.js
@@ -1,20 +1,47 @@
 let canele = 0;
 let furnace = 0;
 
-let buyAudio = new Audio('buy.mp3');
-
 let comments = [];
+
+var task;
+
+window.onload = function () {
+    init();
+}
+
+function init() {
+    console.log("Init page");
+    if (localStorage.getItem("canele") != null) {
+        canele = parseInt(localStorage.getItem("canele"));
+    }
+    if (localStorage.getItem("furnace") != null) {
+        furnace = parseInt(localStorage.getItem("furnace"));
+    }
+    if (localStorage.getItem("comments") != null) {
+        comments = localStorage.getItem("comments").split(",");
+        printComments();
+    }
+    updateBtn();
+}
 
 function incrementCanele() {
     canele = canele + 1 + furnace;
     updateBtn();
+    localStorage.setItem("canele", canele);
 }
 
 function buyFurnace() {
     canele = canele - 25;
     furnace++;
     updateBtn();
-    buyAudio.play();
+    localStorage.setItem("furnace", furnace);
+
+    if (furnace >= 5) {
+        task = window.setInterval(function(){
+            canele++;
+            updateBtn();
+        }, 1000);
+    }
 }
 
 function updateBtn() {
@@ -28,11 +55,17 @@ function updateBtn() {
 }
 
 function addComment() {
-    comments.push(document.getElementById('comment').value);
+    let comm = document.getElementById('comment').value;
+    comments.push(comm);
     document.getElementById('comment').value = "";
-    comments.reverse();
 
+    localStorage.setItem("comments", comments)
+
+    printComments();
+}
+
+function printComments() {
     let comts = "";
-    comments.forEach(value => comts = comts + "<br>" + value);
+    comments.reverse().forEach(value => comts = comts + "<br>" + value);
     document.getElementById('comments').innerHTML = comts;
 }
